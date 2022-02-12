f3312fe82f78d6a4944f7e56de075c2829198991
Voltariuss <loic.duboistermoz@gmail.com>
2022-02-12 19:37:22 +0100

    feat: entich PlayerShop entity definition

--- src/main/java/fr/voltariuss/diagonia/model/entity/PlayerShop.java
+++ src/main/java/fr/voltariuss/diagonia/model/entity/PlayerShop.java
@@ -2,18 +2,21 @@ package fr.voltariuss.diagonia.model.entity;
 
 import fr.voltariuss.diagonia.model.LocationConverter;
 import java.util.UUID;
+import javax.annotation.Nullable;
 import javax.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.jetbrains.annotations.NotNull;
 
 @Entity
 @Table(name = "diagonia_ps_playershop")
 @ToString
 @Getter
-@Setter
+@NoArgsConstructor
 public class PlayerShop {
 
   @Id
@@ -22,16 +25,26 @@ public class PlayerShop {
   private long id;
 
   @Column(nullable = false, unique = true, updatable = false)
+  @Setter
+  @NotNull
   private UUID owner;
 
   @Column(length = 150)
+  @Setter
+  @Nullable
   private String description;
 
+  @Enumerated(EnumType.ORDINAL)
+  @Setter
+  @Nullable
   private Material itemRepresentation;
 
   @Convert(converter = LocationConverter.class)
+  @Setter
+  @Nullable
   private Location tpLocation;
 
   @Column(nullable = false)
+  @Setter
   private boolean isActive;
 }
