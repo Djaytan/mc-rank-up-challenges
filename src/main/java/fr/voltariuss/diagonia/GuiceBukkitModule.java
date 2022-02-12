d0f9f05ffcc49c8fa51b4214025268452e6b4770
Voltariuss <loic.duboistermoz@gmail.com>
2022-02-12 19:20:00 +0100

    build: setup correctly ACF library for commands

--- src/main/java/fr/voltariuss/diagonia/GuiceBukkitModule.java
+++ src/main/java/fr/voltariuss/diagonia/GuiceBukkitModule.java
@@ -1,5 +1,6 @@
 package fr.voltariuss.diagonia;
 
+import co.aikar.commands.PaperCommandManager;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
@@ -10,6 +11,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 import org.bukkit.scoreboard.ScoreboardManager;
 import org.bukkit.structure.StructureManager;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 
 import javax.inject.Named;
@@ -42,43 +44,57 @@ public class GuiceBukkitModule extends AbstractModule {
 
   @Provides
   @Singleton
+  @NotNull
   public Logger provideLogger() {
     return logger;
   }
 
   @Provides
   @Singleton
+  @NotNull
   public PluginManager providePluginManager() {
     return plugin.getServer().getPluginManager();
   }
 
   @Provides
   @Singleton
+  @NotNull
   public ItemFactory provideItemFactory() {
     return plugin.getServer().getItemFactory();
   }
 
   @Provides
   @Singleton
+  @NotNull
   public ConsoleCommandSender provideConsoleCommandSender() {
     return plugin.getServer().getConsoleSender();
   }
 
   @Provides
   @Singleton
+  @NotNull
   public BukkitScheduler provideBukkitScheduler() {
     return plugin.getServer().getScheduler();
   }
 
   @Provides
   @Singleton
+  @NotNull
   public ScoreboardManager provideScoreboardManager() {
     return plugin.getServer().getScoreboardManager();
   }
 
   @Provides
   @Singleton
+  @NotNull
   public StructureManager provideStructureManager() {
     return plugin.getServer().getStructureManager();
+  }
+
+  @Provides
+  @Singleton
+  @NotNull
+  public PaperCommandManager providePaperCommandManager() {
+    return new PaperCommandManager(plugin);
   }
 }
