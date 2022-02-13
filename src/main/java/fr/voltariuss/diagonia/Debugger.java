80be84b7d20a021385a75adeae959ed1652c2fc6
Voltariuss <loic.duboistermoz@gmail.com>
2022-02-13 16:21:22 +0100

    feat: enrich debugger logs

--- src/main/java/fr/voltariuss/diagonia/Debugger.java
+++ src/main/java/fr/voltariuss/diagonia/Debugger.java
@@ -1,15 +1,16 @@
 package fr.voltariuss.diagonia;
 
-import org.slf4j.Logger;
-
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
 
 /** Debugger class for debug logging. */
 @Singleton
 public class Debugger {
 
-  private static final String DEBUG_MODE_PREFIX = "[DEBUG]";
+  private static final String DEBUG_MODE_PREFIX = "[DEBUG] ";
 
   private final Logger logger;
   private final boolean debugMode;
@@ -20,22 +21,45 @@ public class Debugger {
    * @param logger The plugin's logger.
    * @param debugMode "true" if the debug mode for the plugin is enabled, "false" otherwise.
    */
-  public Debugger(Logger logger, @Named("debugMode") boolean debugMode) {
+  @Inject
+  public Debugger(@NotNull Logger logger, @Named("debugMode") boolean debugMode) {
     this.logger = logger;
     this.debugMode = debugMode;
   }
 
-  /**
-   * Log debug messages only if "debugMode" is enabled.
-   *
-   * <p>It's true that varargs are not used here and can lead to lose of performances, but this
-   * happens only during debug phases...
-   *
-   * @param logMessage The message to log in debug mode.
-   */
-  public void debug(String logMessage) {
+  public boolean isDebugEnabled() {
+    return debugMode;
+  }
+
+  public void debug(@NotNull String logMessage) {
     if (debugMode) {
-      logger.info("{} {}", DEBUG_MODE_PREFIX, logMessage);
+      logger.info("{}{}", DEBUG_MODE_PREFIX, logMessage);
+    }
+  }
+
+  public void debug(String logMessage, Object arg) {
+    if (debugMode) {
+      logger.info("{}{} {}", DEBUG_MODE_PREFIX, logMessage, arg);
+    }
+  }
+
+  public void debug(String logMessage, Object arg1, Object arg2) {
+    if (debugMode) {
+      logger.info("{}{} {} {}", DEBUG_MODE_PREFIX, logMessage, arg1, arg2);
+    }
+  }
+
+  @SuppressWarnings({"java:S2629", "java:S3457"})
+  public void debug(String logMessage, Object... args) {
+    if (debugMode) {
+      logger.info(DEBUG_MODE_PREFIX + logMessage, args);
+    }
+  }
+
+  @SuppressWarnings({"java:S2629", "java:S3457"})
+  public void debug(String logMessage, Throwable cause) {
+    if (debugMode) {
+      logger.info(DEBUG_MODE_PREFIX + logMessage, cause);
     }
   }
 }
