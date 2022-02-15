1064facdab0cd4349b142449ea8dfa2517e8bf7a
Voltariuss <loic.duboistermoz@gmail.com>
2022-02-15 19:42:09 +0100

    doc: finalized documentation

--- src/main/java/fr/voltariuss/diagonia/model/AbstractJpaDao.java
+++ src/main/java/fr/voltariuss/diagonia/model/AbstractJpaDao.java
@@ -7,7 +7,13 @@ import org.hibernate.Transaction;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * TODO: Doc
+ * Abstract JPA DAO class to be inherited by entities' DAO classes.
+ *
+ * <p>The purpose of this class is to make generic the management of sessions and transactions
+ * without the need to set up a utility class or duplicate the logic over each DAO class.
+ *
+ * The management or sessions and transactions is in charge of the service layer. The DAO layer must
+ * only call the {@link #getCurrentSession()} method and that's it.
  *
  * @param <T> The entity type.
  * @param <I> The ID type of the entity.
@@ -29,6 +35,8 @@ public abstract class AbstractJpaDao<T, I extends Serializable> implements JpaDa
   /**
    * Gets the current session recently opened.
    *
+   * <p>A session must be opened before calling this method.
+   *
    * @return The current session recently opened.
    * @throws JpaDaoException If no session have been opened before recovering one.
    */
@@ -41,6 +49,9 @@ public abstract class AbstractJpaDao<T, I extends Serializable> implements JpaDa
 
   /**
    * Begins a transaction for the current session.
+   *
+   * <p>Must be used after a call to {@link #openSession()} and only one transaction can up at the
+   * same time for a session.
    *
    * @return The began transaction for the current session.
    * @throws JpaDaoException If no session have been opened before beginning a transaction.
@@ -59,6 +70,8 @@ public abstract class AbstractJpaDao<T, I extends Serializable> implements JpaDa
   /**
    * Opens a new session.
    *
+   * <p>Only one session can be up at the same time.
+   *
    * @throws JpaDaoException If a session still opened at creation time of another one.
    */
   public void openSession() throws JpaDaoException {
@@ -72,6 +85,8 @@ public abstract class AbstractJpaDao<T, I extends Serializable> implements JpaDa
 
   /**
    * Destroys the current session.
+   *
+   * <p>A session must be opened before destroying it, otherwise it is considered as an error.
    *
    * @throws JpaDaoException If no session still opened at destroy time.
    */
