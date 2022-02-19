774ce2a76a645bf3cfeb9c986d79becd1a349d2e
Voltariuss <loic.duboistermoz@gmail.com>
2022-02-19 15:28:57 +0100

    add populate sql script for dev purposes

---
+++ src/main/resources/META-INF/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO diagonia_ps_playershop (ps_id, ps_description, ps_is_active, ps_item_icon, ps_owner_uuid, ps_tp_location)
+VALUES (1, NULL, FALSE, NULL, '2defdbfc-1548-4f55-baae-0346482a087c', NULL),
+       (2,
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempus mollis ipsum, eget ultrices velit egestas id. Donec ultrices ut nisl in egestas. Aenean fermentum tincidunt purus, vitae commodo sem maximus in. Vivamus in neque nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin molestie leo eget tellus vulputate interdum.',
+        TRUE, NULL, 'c06a4238-6268-44be-adee-ad53bcb0b198', NULL)
