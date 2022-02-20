b667258760ee48704e9e3162c21aa937e0b08639
Voltariuss <loic.duboistermoz@gmail.com>
2022-02-20 19:50:28 +0100

    feat: add feedback at buy playershop time

--- src/main/java/fr/voltariuss/diagonia/controller/PlayerShopController.java
+++ src/main/java/fr/voltariuss/diagonia/controller/PlayerShopController.java
@@ -1,12 +1,16 @@
 package fr.voltariuss.diagonia.controller;
 
+import fr.voltariuss.diagonia.PluginConfig;
 import fr.voltariuss.diagonia.model.entity.PlayerShop;
 import fr.voltariuss.diagonia.model.service.PlayerShopService;
 import fr.voltariuss.diagonia.view.gui.PlayerShopGui;
 import java.util.List;
+import java.util.ResourceBundle;
 import javax.inject.Inject;
 import javax.inject.Provider;
 import javax.inject.Singleton;
+import net.kyori.adventure.text.Component;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
@@ -14,18 +18,27 @@ import org.slf4j.Logger;
 @Singleton
 public class PlayerShopController {
 
+  private final Economy economy;
   private final Logger logger;
   private final Provider<PlayerShopGui> playerShopGui;
   private final PlayerShopService playerShopService;
+  private final PluginConfig pluginConfig;
+  private final ResourceBundle resourceBundle;
 
   @Inject
   public PlayerShopController(
+      @NotNull Economy economy,
       @NotNull Logger logger,
       @NotNull Provider<PlayerShopGui> playerShopGui,
-      @NotNull PlayerShopService playerShopService) {
+      @NotNull PlayerShopService playerShopService,
+      @NotNull PluginConfig pluginConfig,
+      @NotNull ResourceBundle resourceBundle) {
+    this.economy = economy;
     this.logger = logger;
     this.playerShopGui = playerShopGui;
     this.playerShopService = playerShopService;
+    this.pluginConfig = pluginConfig;
+    this.resourceBundle = resourceBundle;
   }
 
   public void openPlayerShop(@NotNull Player player) {
@@ -38,5 +51,11 @@ public class PlayerShopController {
     logger.info("Buy of a playershop for player {}", player.getName());
     PlayerShop ps = new PlayerShop(player.getUniqueId());
     playerShopService.persist(ps);
+    player.sendMessage(
+        Component.text(
+            String.format(
+                resourceBundle.getString("diagonia.playershop.buy.successfully_bought"),
+                economy.format(pluginConfig.getPlayerShopConfig().getBuyCost()),
+                economy.currencyNamePlural())));
   }
 }
