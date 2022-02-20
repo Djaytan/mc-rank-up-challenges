e0c054690a8170bc308561ef958ff429a21fb592
Voltariuss <loic.duboistermoz@gmail.com>
2022-02-20 14:21:52 +0100

    WIP: playershop inventory

--- src/main/java/fr/voltariuss/diagonia/view/gui/PlayerShopGui.java
+++ src/main/java/fr/voltariuss/diagonia/view/gui/PlayerShopGui.java
@@ -1,7 +1,9 @@
 package fr.voltariuss.diagonia.view.gui;
 
 import dev.triumphteam.gui.guis.Gui;
+import dev.triumphteam.gui.guis.GuiItem;
 import fr.voltariuss.diagonia.model.entity.PlayerShop;
+import fr.voltariuss.diagonia.view.item.BuyPlayerShopItem;
 import fr.voltariuss.diagonia.view.item.PlayerShopItem;
 import java.util.List;
 import javax.inject.Inject;
@@ -13,17 +15,21 @@ import org.jetbrains.annotations.NotNull;
 @Singleton
 public class PlayerShopGui {
 
-  private final PlayerShopItem playerShopItemFactory;
+  private final BuyPlayerShopItem buyPlayerShopItem;
+  private final PlayerShopItem playerShopItem;
 
   @Inject
-  public PlayerShopGui(@NotNull PlayerShopItem playerShopItemFactory) {
-    this.playerShopItemFactory = playerShopItemFactory;
+  public PlayerShopGui(@NotNull BuyPlayerShopItem buyPlayerShopItem, @NotNull PlayerShopItem playerShopItem) {
+    this.buyPlayerShopItem = buyPlayerShopItem;
+    this.playerShopItem = playerShopItem;
   }
 
   public void open(@NotNull Player whoOpen, List<PlayerShop> playerShopList) {
     Gui gui = Gui.gui().title(Component.text("PlayerShop")).rows(6).create();
 
-    playerShopList.forEach(playerShopItemFactory::createItem);
+    gui.addItem(
+        playerShopList.stream().map(playerShopItem::createItem).toArray(GuiItem[]::new));
+    gui.setItem(6, 5, buyPlayerShopItem.createItem());
 
     gui.setDefaultClickAction(event -> event.setCancelled(true));
 
