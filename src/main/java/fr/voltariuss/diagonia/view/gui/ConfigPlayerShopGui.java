e9a30ef5257b6199e6723a612c56a272c4ea533a
Voltariuss <loic.duboistermoz@gmail.com>
2022-02-22 20:14:24 +0100

    feat(playershop): add go to previous gui button

--- src/main/java/fr/voltariuss/diagonia/view/gui/ConfigPlayerShopGui.java
+++ src/main/java/fr/voltariuss/diagonia/view/gui/ConfigPlayerShopGui.java
@@ -1,22 +1,29 @@
 package fr.voltariuss.diagonia.view.gui;
 
+import dev.triumphteam.gui.builder.item.ItemBuilder;
 import dev.triumphteam.gui.guis.Gui;
+import fr.voltariuss.diagonia.controller.PlayerShopController;
 import fr.voltariuss.diagonia.model.entity.PlayerShop;
 import fr.voltariuss.diagonia.view.item.ActivationPlayerShopItem;
 import fr.voltariuss.diagonia.view.item.DefineTpPlayerShopItem;
 import java.util.ResourceBundle;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 @Singleton
 public class ConfigPlayerShopGui {
 
+  private static final Material PREVIOUS_GUI_MATERIAL = Material.ARROW;
+
   private final ActivationPlayerShopItem activationPlayerShopItem;
   private final DefineTpPlayerShopItem defineTpPlayerShopItem;
   private final MiniMessage miniMessage;
+  private final PlayerShopController playerShopController;
   private final ResourceBundle resourceBundle;
 
   @Inject
@@ -24,9 +31,11 @@ public class ConfigPlayerShopGui {
       @NotNull ActivationPlayerShopItem activationPlayerShopItem,
       @NotNull DefineTpPlayerShopItem defineTpPlayerShopItem,
       @NotNull MiniMessage miniMessage,
+      @NotNull PlayerShopController playerShopController,
       @NotNull ResourceBundle resourceBundle) {
     this.activationPlayerShopItem = activationPlayerShopItem;
     this.defineTpPlayerShopItem = defineTpPlayerShopItem;
+    this.playerShopController = playerShopController;
     this.miniMessage = miniMessage;
     this.resourceBundle = resourceBundle;
   }
@@ -40,10 +49,19 @@ public class ConfigPlayerShopGui {
                     resourceBundle.getString("diagonia.playershop.config.gui.title")))
             .create();
 
-    // TODO
-
     gui.setItem(2, 4, activationPlayerShopItem.createItem(playerShop));
     gui.setItem(2, 6, defineTpPlayerShopItem.createItem(playerShop));
+
+    gui.setItem(
+        3,
+        1,
+        ItemBuilder.from(PREVIOUS_GUI_MATERIAL)
+            .name(
+                miniMessage
+                    .deserialize(resourceBundle.getString("diagonia.gui.go_to_previous"))
+                    .decoration(TextDecoration.ITALIC, false))
+            .asGuiItem(
+                event -> playerShopController.openPlayerShop((Player) event.getWhoClicked())));
 
     gui.setDefaultClickAction(event -> event.setCancelled(true));
     gui.open(whoOpen);
