3314bc5de546debf965f76295739bb927db43854
Voltariuss <loic.duboistermoz@gmail.com>
2022-04-01 23:07:27 +0200

    chore: use license Apache 2.0 in project and all source files

--- src/main/java/fr/voltariuss/diagonia/guice/GuiceGeneralModule.java
+++ src/main/java/fr/voltariuss/diagonia/guice/GuiceGeneralModule.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2022 - Loïc DUBOIS-TERMOZ
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package fr.voltariuss.diagonia.guice;
 
 import com.google.inject.AbstractModule;
@@ -14,8 +30,8 @@ import fr.voltariuss.diagonia.model.service.EconomyService;
 import fr.voltariuss.diagonia.model.service.EconomyVaultService;
 import fr.voltariuss.diagonia.model.service.JobsRebornService;
 import fr.voltariuss.diagonia.model.service.JobsService;
-import fr.voltariuss.diagonia.model.service.RankService;
 import fr.voltariuss.diagonia.model.service.RankLuckPermsService;
+import fr.voltariuss.diagonia.model.service.RankService;
 import java.util.Objects;
 import java.util.ResourceBundle;
 import javax.inject.Named;
