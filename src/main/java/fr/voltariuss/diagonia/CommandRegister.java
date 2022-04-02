97c037d7be4667a494990a5dc32d765e8bc140c6
Voltariuss <loic.duboistermoz@gmail.com>
2022-04-02 18:24:00 +0200

    refactor: remove /uuid command

--- src/main/java/fr/voltariuss/diagonia/CommandRegister.java
+++ src/main/java/fr/voltariuss/diagonia/CommandRegister.java
@@ -19,7 +19,6 @@ package fr.voltariuss.diagonia;
 import co.aikar.commands.PaperCommandManager;
 import fr.voltariuss.diagonia.view.command.PlayerShopCommand;
 import fr.voltariuss.diagonia.view.command.RanksCommand;
-import fr.voltariuss.diagonia.view.command.UuidCommand;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.jetbrains.annotations.NotNull;
@@ -31,23 +30,19 @@ public class CommandRegister {
 
   private final PlayerShopCommand playerShopCommand;
   private final RanksCommand ranksCommand;
-  private final UuidCommand uuidCommand;
 
   @Inject
   public CommandRegister(
       @NotNull PaperCommandManager paperCommandManager,
       @NotNull PlayerShopCommand playerShopCommand,
-      @NotNull RanksCommand ranksCommand,
-      @NotNull UuidCommand uuidCommand) {
+      @NotNull RanksCommand ranksCommand) {
     this.paperCommandManager = paperCommandManager;
     this.playerShopCommand = playerShopCommand;
     this.ranksCommand = ranksCommand;
-    this.uuidCommand = uuidCommand;
   }
 
   public void registerCommands() {
     paperCommandManager.registerCommand(playerShopCommand);
     paperCommandManager.registerCommand(ranksCommand);
-    paperCommandManager.registerCommand(uuidCommand);
   }
 }
