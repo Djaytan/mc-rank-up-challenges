ceb1c517d89fd90c67b8bb49702fd2ac2e3ed15d
Voltariuss <loic.duboistermoz@gmail.com>
2022-05-01 18:09:32 +0200

    refactor(playershop): separate interface from implementation

--- src/main/java/fr/voltariuss/diagonia/model/service/PlayerShopService.java
+++ src/main/java/fr/voltariuss/diagonia/model/service/PlayerShopService.java
@@ -16,121 +16,21 @@
 
 package fr.voltariuss.diagonia.model.service;
 
-import fr.voltariuss.diagonia.RemakeBukkitLogger;
-import fr.voltariuss.diagonia.model.dao.JpaDaoException;
-import fr.voltariuss.diagonia.model.dao.PlayerShopDao;
 import fr.voltariuss.diagonia.model.entity.PlayerShop;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import javax.inject.Inject;
-import javax.inject.Singleton;
-import org.hibernate.HibernateException;
-import org.hibernate.Transaction;
 import org.jetbrains.annotations.NotNull;
 
-@Singleton
-public class PlayerShopService {
+public interface PlayerShopService {
 
-  private static final String TRANSACTION_ROLLBACK_FAIL_MESSAGE = "Failed to rollback transaction";
+  void persist(@NotNull PlayerShop playerShop);
 
-  private final RemakeBukkitLogger logger;
-  private final PlayerShopDao playerShopDao;
+  void update(@NotNull PlayerShop playerShop);
 
-  @Inject
-  public PlayerShopService(@NotNull RemakeBukkitLogger logger, @NotNull PlayerShopDao playerShopDao) {
-    this.logger = logger;
-    this.playerShopDao = playerShopDao;
-  }
+  @NotNull Optional<PlayerShop> findById(long id);
 
-  public void persist(@NotNull PlayerShop playerShop) {
-    playerShopDao.openSession();
-    Transaction tx = playerShopDao.beginTransaction();
-    try {
-      playerShopDao.persist(playerShop);
-      tx.commit();
-      logger.debug("PlayerShop persisted: {}", playerShop);
-    } catch (HibernateException e) {
-      try {
-        tx.rollback();
-      } catch (RuntimeException re) {
-        throw new JpaDaoException(TRANSACTION_ROLLBACK_FAIL_MESSAGE, re);
-      }
-      throw e;
-    } finally {
-      playerShopDao.destroySession();
-    }
-  }
+  @NotNull Optional<PlayerShop> findByUuid(@NotNull UUID uuid);
 
-  public void update(@NotNull PlayerShop playerShop) {
-    playerShopDao.openSession();
-    Transaction tx = playerShopDao.beginTransaction();
-    try {
-      playerShopDao.update(playerShop);
-      tx.commit();
-      logger.debug("PlayerShop updated: {}", playerShop);
-    } catch (HibernateException e) {
-      try {
-        tx.rollback();
-      } catch (RuntimeException re) {
-        throw new JpaDaoException(TRANSACTION_ROLLBACK_FAIL_MESSAGE, re);
-      }
-      throw e;
-    } finally {
-      playerShopDao.destroySession();
-    }
-  }
-
-  public @NotNull Optional<PlayerShop> findById(long id) {
-    playerShopDao.openSession();
-    try {
-      Optional<PlayerShop> playerShop = playerShopDao.findById(id);
-      logger.debug("PlayerShop found for ID {}: {}", id, playerShop);
-      return playerShop;
-    } finally {
-      playerShopDao.destroySession();
-    }
-  }
-
-  public @NotNull Optional<PlayerShop> findByUuid(@NotNull UUID uuid) {
-    playerShopDao.openSession();
-    try {
-      Optional<PlayerShop> playerShop = playerShopDao.findByUuid(uuid);
-      logger.debug("PlayerShop found for UUID {}: {}", uuid, playerShop);
-      return playerShop;
-    } finally {
-      playerShopDao.destroySession();
-    }
-  }
-
-  public void delete(@NotNull PlayerShop playerShop) {
-    playerShopDao.openSession();
-    Transaction tx = playerShopDao.beginTransaction();
-    try {
-      long idPlayerShop = playerShop.getId();
-      playerShopDao.delete(playerShop);
-      tx.commit();
-      logger.debug("PlayerShop deleted: id={}", idPlayerShop);
-    } catch (HibernateException e) {
-      try {
-        tx.rollback();
-      } catch (RuntimeException re) {
-        throw new JpaDaoException(TRANSACTION_ROLLBACK_FAIL_MESSAGE, re);
-      }
-      throw e;
-    } finally {
-      playerShopDao.destroySession();
-    }
-  }
-
-  public @NotNull List<PlayerShop> findAll() {
-    playerShopDao.openSession();
-    try {
-      List<PlayerShop> playerShops = playerShopDao.findAll();
-      logger.debug("PlayerShop find all: {}", playerShops);
-      return playerShops;
-    } finally {
-      playerShopDao.destroySession();
-    }
-  }
+  @NotNull List<PlayerShop> findAll();
 }
