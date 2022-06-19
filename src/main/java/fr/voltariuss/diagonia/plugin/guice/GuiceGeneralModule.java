43a0aa11ac38b7c7e86195454979a014989f3556
Voltariuss <loic.duboistermoz@gmail.com>
2022-06-19 23:19:51 +0200

    refactor: move jobs reborn place break patch into another repository

--- src/main/java/fr/voltariuss/diagonia/plugin/guice/GuiceGeneralModule.java
+++ src/main/java/fr/voltariuss/diagonia/plugin/guice/GuiceGeneralModule.java
@@ -20,14 +20,12 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
 import fr.voltariuss.diagonia.controller.api.ConfigController;
-import fr.voltariuss.diagonia.controller.api.JobsController;
 import fr.voltariuss.diagonia.controller.api.MessageController;
 import fr.voltariuss.diagonia.controller.api.PlayerController;
 import fr.voltariuss.diagonia.controller.api.PluginController;
 import fr.voltariuss.diagonia.controller.api.RankUpChallengesController;
 import fr.voltariuss.diagonia.controller.api.RankUpController;
 import fr.voltariuss.diagonia.controller.implementation.ConfigControllerImpl;
-import fr.voltariuss.diagonia.controller.implementation.JobsControllerImpl;
 import fr.voltariuss.diagonia.controller.implementation.MessageControllerImpl;
 import fr.voltariuss.diagonia.controller.implementation.PlayerControllerImpl;
 import fr.voltariuss.diagonia.controller.implementation.PluginControllerImpl;
@@ -52,7 +50,6 @@ public class GuiceGeneralModule extends AbstractModule {
   public void configure() {
     bind(ConfigController.class).to(ConfigControllerImpl.class);
     bind(EconomyService.class).to(EconomyVaultServiceImpl.class);
-    bind(JobsController.class).to(JobsControllerImpl.class);
     bind(JobsService.class).to(JobsRebornServiceImpl.class);
     bind(MessageController.class).to(MessageControllerImpl.class);
     bind(PlayerController.class).to(PlayerControllerImpl.class);
